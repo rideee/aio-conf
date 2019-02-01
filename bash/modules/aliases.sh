c593fc73b1991b63e7d9d7987d088ca0552ebdea
Michał Kątnik <michal.katnik91@gmail.com>
2019-02-01 23:17:11 +0100

    Init

---
+++ bash/modules/aliases.sh
@@ -0,0 +1,21 @@
+alias ':q'='exit'
+alias vi='nvim'
+alias tree='tree -C'
+alias grep='grep --color=auto'
+alias grep='egrep --color=auto'
+alias grep='fgrep --color=auto'
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias l='ls -lah'
+alias pacman='sudo pacman'
+alias apm='apm --color'
+alias systemctl='sudo systemctl'
+alias hibernate='systemctl hibernate'
+alias mount-ridATriddev='sshfs rid@riddev.eu:/home/rid /mnt/ridATriddev'
+alias umount-ridATriddev='sudo umount /mnt/ridATriddev'
+alias mount-riddevATriddev='sshfs riddev@riddev.eu:/var/www/html/riddev/public /mnt/riddevATriddev'
+alias umount-riddevATriddev='sudo umount /mnt/riddevATriddev'
+alias rgoimp="/home/rid/.rid/sh/rid-goimports.sh"
+alias serve="browser-sync start --server --files \"css/*.css, *.css, *.html\""
+alias testme="echo OK"
+
